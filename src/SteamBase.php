72946b4757b77c6cc8b276b3edeaddc05c87179b
Daniel Brendel <dbrendel1988@gmail.com>
2021-04-04 12:30:42 +0200

    Use cURL instead of GuzzleHTTP

--- src/SteamBase.php
+++ src/SteamBase.php
@@ -21,4 +21,26 @@ class SteamBase {
     {
         return $this->apiKey;
     }
+
+    protected function queryResource($url)
+    {
+        try {
+            $handle = curl_init($url);
+
+            curl_setopt($handle, CURLOPT_HEADER, false);
+            curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+
+            $response = curl_exec($handle);
+
+            if(curl_error($handle) !== '') {
+                throw new Exception(curl_error($handle));
+            }
+
+            curl_close($handle);
+
+            return json_decode($response);
+        } catch (Exception $e) {
+            throw $e;
+        }
+    }
 }
